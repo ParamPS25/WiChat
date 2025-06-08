b1ca977e661cb1b3e6ce1a34a67da3ff8302f895
ParamPS25 <bhavsarparam1940@gmail.com>
2025-06-08 18:36:07 +0530

    updated date and time format

--- frontend/src/lib/dateFormat.js
+++ frontend/src/lib/dateFormat.js
@@ -1,7 +1,10 @@
-export default function formatMessageTime(date){
-    return new Date(date).toLocaleTimeString("en-IN",{
-        hour : "2-digit",
-        minute : "2-digit",
-        hour12 : false
-    })
+export default function formatMessageTime(dateStr) {
+    const date = new Date(dateStr);
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = date.toLocaleString('en-IN', { month: 'short' }); // Jun
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+
+    return `${day} ${month}, ${hour}:${minute}`;
 }
