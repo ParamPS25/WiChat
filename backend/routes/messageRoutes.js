122795b006acc524e134de5bf33343260da56f73
ParamPS25 <bhavsarparam1940@gmail.com>
2025-06-29 01:44:07 +0530

    fix : route conflict issues

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -5,13 +5,16 @@ import { getMessages, getUsersForSidebar, sendMessage, getUnreadMessageCounts, m
 
 const router = express.Router();
 
-router.get('/unread-count',protectRoute,getUnreadMessageCounts);
-
+// Specific first
+router.get('/unread-count', protectRoute, getUnreadMessageCounts);
 router.get('/users', protectRoute , getUsersForSidebar);
-router.get('/:id', protectRoute , getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
-
-
 router.patch("/mark-read/:id", protectRoute, markMessagesAsRead);
+
+// Placing this last to avoid conflicts
+router.get('/:id', protectRoute , getMessages);
+
+// Why this fixes the crash
+// path-to-regexp tries to parse /unread-count as if it’s the :id parameter (because /:id was defined first), and fails to compile the route.
 
 export default router;
