c09c3da25cd0d703ad37f96d6960fca10fea4c96
ParamPS25 <bhavsarparam1940@gmail.com>
2025-06-29 01:00:12 +0530

    deploy changes : deploying both on single domain of render instead of seperate domain

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,10 @@ import authRoutes from './routes/authRoutes.js'
 import messageRoutes from './routes/messageRoutes.js'
 
 import { app, server } from './lib/socket.js';
+import path from 'path';
 
 const PORT = process.env.PORT || 8000
+const __dirname = path.resolve();
 
 app.use(cors({
     origin : process.env.CLIENT_URL,
@@ -32,6 +34,14 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });         
+}
 
 server.listen(PORT , () => {
     console.log(`Server running on ${PORT}`)
